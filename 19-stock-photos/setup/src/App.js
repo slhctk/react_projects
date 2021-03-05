0b48595ef6b9c3fb6cba3cf4d1d2732d832feebe
slhctk <salihcitak@yahoo.com>
2021-03-05 16:24:57 -0500

    .env done

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const[loading, setLoading]= useState(false);
+  const[photos, setPhotos]= useState([]);
+
+  const fetchImages= async()=>{
+    setLoading(true);
+    let url;
+    url= `${mainUrl}${clientID}`
+    try {
+      const response= await fetch(url);
+      const data= await response.json();
+      setPhotos= (data);
+      setLoading=(false);
+    } catch (error) {
+      setLoading(false);
+     console.log(error) 
+    }
+  }
+
+  useEffect(()=>{
+    fetchImages();
+  },[])
+
+  const handleSubmit= (e)=>{
+    e.preventDefault();
+    console.log('Hello')
+  }
+
+  return <main>
+    <section className="search">
+      <form className="search-form">
+        <input type="text" placeholder='search' className='form-input'/>
+        <button type='submit' className='submit-btn' onClick={handleSubmit}>
+          <FaSearch/>
+        </button>
+      </form>
+    </section>
+    <section className="photos">
+      <div className="photos ceneter">
+        {photos.map((image, index)=>{
+          return <Photo key={index} {...image}/>
+        })}
+      </div>
+    </section>
+  </main>
 }
 
 export default App
