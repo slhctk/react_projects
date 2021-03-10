01e88485f6fa4538ce0ffff0e498a95a64612c0a
slhctk <salihcitak@yahoo.com>
2021-03-10 15:59:01 -0500

    momentjs

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -8,15 +8,34 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const[loading, setLoading]= useState(false);
   const[photos, setPhotos]= useState([]);
+  const[pages, setPages]= useState(0);
+  const[query, setQuery]=useState('');
 
   const fetchImages= async()=>{
     setLoading(true);
     let url;
-    url= `${mainUrl}${clientID}`
+    const urlPage= `&page=${pages}`;
+    const urlQuery= `&query=${query}`;
+    if(query){
+      url=`${searchUrl}${clientID}${urlPage}${urlQuery}`
+    }
+    else{
+      url= `${mainUrl}${clientID}${urlPage}`
+    }
+    url= `${mainUrl}${clientID}${urlPage}`
     try {
       const response= await fetch(url);
       const data= await response.json();
-      setPhotos(data);
+      setPhotos((oldPhotos)=>{
+        if(query && pages===1){
+          return data.results
+        }
+        else if(query)
+        return [...oldPhotos, ...data.results]
+        else{
+          return [...oldPhotos, ...data]
+        }
+      });
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -26,41 +45,48 @@ function App() {
 
   useEffect(()=>{
     fetchImages();
-  },[])
+  },[pages])
   useEffect(()=>{
     const event= window.addEventListener('scroll',()=>{
     //  console.log(`innerHeight ${window.innerHeight}`)
     //  console.log(`scrollY ${window.scrollY}`) 
     //  console.log(`body height ${document.body.scrollHeight}`) 
-     if((window.innerHeight+window.scrollY)>=document.body.scrollHeight-2)
-     {console.log('it worked')}
+     if(!loading && (window.innerHeight+window.scrollY)>=document.body.scrollHeight-2)
+     {setPages((oldPage)=>{
+      return oldPage+1
+     })}
     });
     return()=> window.removeEventListener('scroll', event)
   },[])
 
   const handleSubmit= (e)=>{
     e.preventDefault();
-    console.log('Hello')
+    setPages(1);
+    
   }
 
   return <main>
     <section className="search">
       <form className="search-form">
-        <input type="text" placeholder='search' className='form-input'/>
+        <input type="text"
+        placeholder='search'
+         className='form-input'
+        value={query} 
+        onChange={(e)=>setQuery(e.target.value)}
+        />
         <button type='submit' className='submit-btn' onClick={handleSubmit}>
           <FaSearch/>
         </button>
       </form>
     </section>
-    <section className="photos">
-      <div className="photos-center">
-        {photos.map((image)=>{
-         
-          return <Photo key={image.id} {...image}/>
-        })}
-      </div>
-      {loading && <h2 className='loading'>Loading...</h2>}
-    </section>
+    <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
   </main>
 }
 
